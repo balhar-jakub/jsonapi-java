481d8e00dbe9236ab3ec004b4991659bf907c7ba
Jakub Balhar <balhar.jakub@gmail.com>
2015-05-27 13:41:59 +0200

    Null value in included is correctly ignored test

--- src/test/java/net/balhar/jsonapi/simple/IncludedProperty.java
+++ src/test/java/net/balhar/jsonapi/simple/IncludedProperty.java
@@ -1,11 +1,13 @@
 package net.balhar.jsonapi.simple;
 
+import net.avh4.test.junit.Nested;
 import net.balhar.jsonapi.ApiKeys;
 import net.balhar.jsonapi.Document;
 import net.balhar.jsonapi.Identifiable;
 import net.balhar.jsonapi.Included;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -19,48 +21,71 @@ import static org.junit.Assert.assertThat;
  * Test whether included works properly.
  * As part of including attach the resources into the linkage section of given resource?
  */
+@RunWith(Nested.class)
 public class IncludedProperty {
-    private TransformedDocument warriorTransfer;
+    TransformedDocument warriorTransfer;
 
-    @Before
-    public void setUp(){
-        Collection<Elf> enemies = new ArrayList<>();
-        enemies.add(new Elf("uuid1", "Thranduil"));
-        enemies.add(new Elf("uuid2", "Legolas"));
-        Dwarf universalWarrior = new Dwarf("uuid1", "Gimli", enemies);
+    public class ValidIncludedProperty {
+        @Before
+        public void setUp() {
+            Collection<Elf> enemies = new ArrayList<>();
+            enemies.add(new Elf("uuid1", "Thranduil"));
+            enemies.add(new Elf("uuid2", "Legolas"));
+            Dwarf universalWarrior = new Dwarf("uuid1", "Gimli", enemies);
 
-        Document document = new SimpleDocument(universalWarrior);
-        warriorTransfer = (TransformedDocument) document.transform();
+            Document document = new SimpleDocument(universalWarrior);
+            warriorTransfer = (TransformedDocument) document.transform();
+        }
+
+        @Test
+        public void includedIsMissingInPayload() {
+            Map transferredWarrior = (Map) warriorTransfer.data().iterator().next();
+            assertThat(transferredWarrior.get("enemies"), is(nullValue()));
+        }
+
+        @Test
+        public void includedIsInLinkageInLinks() throws Exception {
+            TransformedResource transferredWarrior = (TransformedResource) warriorTransfer.data().iterator().next();
+            Map linksToEnemies = transferredWarrior.links("Elf");
+            Collection linkage = (Collection) linksToEnemies.get(ApiKeys.LINKAGE);
+            assertThat(linkage.size(), is(2));
+
+            Elf enemy = (Elf) linkage.iterator().next();
+            String type = (String) enemy.getClass().getDeclaredField(ApiKeys.TYPE).get(enemy);
+            assertThat(type, is("Elf"));
+            assertThat(enemy.getUuid(), is("uuid1"));
+        }
+
+        @Test
+        public void includedIsPresentInIncludedSection() throws Exception {
+            assertThat(warriorTransfer.included().size(), is(2));
+            Elf includedContainsType = (Elf) warriorTransfer.included().iterator().next();
+            String type = (String) includedContainsType.getClass().getDeclaredField(ApiKeys.TYPE).get(includedContainsType);
+            assertThat(type, is("Elf"));
+        }
     }
 
-    @Test
-    public void includedIsMissingInPayload(){
-        Map transferredWarrior = (Map) warriorTransfer.data().iterator().next();
-        assertThat(transferredWarrior.get("enemies"), is(nullValue()));
-    }
+    public class NullProperty {
+        @Before
+        public void setUp() {
+            Dwarf universalWarrior = new Dwarf("uuid1", "Gimli", null);
 
-    @Test
-    public void includedIsInLinkageInLinks() throws Exception {
-        TransformedResource transferredWarrior = (TransformedResource) warriorTransfer.data().iterator().next();
-        Map linksToEnemies = transferredWarrior.links("Elf");
-        Collection linkage = (Collection) linksToEnemies.get(ApiKeys.LINKAGE);
-        assertThat(linkage.size(), is(2));
+            Document document = new SimpleDocument(universalWarrior);
+            warriorTransfer = (TransformedDocument) document.transform();
+        }
 
-        Elf enemy = (Elf) linkage.iterator().next();
-        String type = (String) enemy.getClass().getDeclaredField(ApiKeys.TYPE).get(enemy);
-        assertThat(type, is("Elf"));
-        assertThat(enemy.getUuid(), is("uuid1"));
-    }
-
-    @Test
-    public void includedIsPresentInIncludedSection(){
-        assertThat(warriorTransfer.included().size(), is(2));
+        @Test
+        public void includedIsntPresent() {
+            assertThat(warriorTransfer.included(), nullValue());
+        }
     }
 }
 
 class Dwarf implements Identifiable {
     private String uuid;
     private String name;
+    @Included(type="Elf")
+    private Elf mortalEnemy;
     @Included(type = "Elf")
     private Collection<Elf> enemies;
 
